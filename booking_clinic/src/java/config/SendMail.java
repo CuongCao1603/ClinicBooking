227fc59c75501a19ffc83c07e97ce5a0606a0ba2
vanhvanh <vanhleg2301@gmail.com>
2023-06-05 11:59:09 +0700

    new

---
+++ booking_clinic/src/java/config/SendMail.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author DELL
+ */
+public class SendMail {
+//
+//    public static ResourceBundle bundle = ResourceBundle.getBundle("resource.email");
+//    
+//    public static void setContent(String username, String code, String email) {
+//        String subject = "[Doctris] Please verify your email.";
+//        String message = "<!DOCTYPE html>\n"
+//                + "<html lang=\"en\">\n "
+//                + "\n"
+//                + "<head>\n "
+//                + "</head>\n"
+//                + "\n"
+//                + "<body>\n"
+//                + "    <h3 style=\"color: blue;\">Xin chào " + username + " !</h3>\n"
+//                + "    <div>Link xác minh tài khoản của bạn là : <a href=\"" + code + "\">Nhấn vào đây!</a></div>\n"
+//                + "    <div>Thư này được tạo ra tự động.</div>\n"
+//                + "    <div>Nếu bạn cần trợ giúp hoặc có câu hỏi, hãy gửi email đến doctris.care@gmail.com bất cứ lúc nào.</div>\n"
+//                + "    <h3 style=\"color: blue;\">Trân trọng!</h3>\n"
+//                + "\n"
+//                + "</body>\n"
+//                + "\n"
+//                + "</html>";
+//        SendMail.send(email, subject, message, bundle.getString("email"), bundle.getString("pass"));
+//    }
+}
