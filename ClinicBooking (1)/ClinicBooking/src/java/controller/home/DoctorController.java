88c6413b3aa1e378f229b1eaa79edd3a13caed76
vanhvanh <vanhleg2301@gmail.com>
2023-06-26 23:44:41 +0700

    new

--- ClinicBooking (1)/ClinicBooking/src/java/controller/home/DoctorController.java
+++ ClinicBooking (1)/ClinicBooking/src/java/controller/home/DoctorController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller.home;
 
 import dal.DoctorDAO;
@@ -60,6 +56,8 @@ public class DoctorController extends HttpServlet {
                 getdoctor = doctordao.getAllDoctorHome();
             }
 
+
+
             if (action.equals("sort")) {
                 String type = request.getParameter("type");
                 request.setAttribute("sort", type);
@@ -70,6 +68,7 @@ public class DoctorController extends HttpServlet {
                 }
                 url = "doctor?action=sort&type=" + type;
             }
+
 
             if (action.equals("filter")) {
                 String gender = request.getParameter("gender");
